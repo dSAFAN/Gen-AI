05521d5960aca16d2dfb351f69d28f82eeb4b549
Safan Dar <safandar2@gmail.com>
2026-06-01 15:33:08 +0530

    used langchain message roles and created an array for message history

--- chatmodels/dev_chatBot.py
+++ chatmodels/dev_chatBot.py
@@ -1,17 +1,25 @@
 from langchain_mistralai import ChatMistralAI
+from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
 from dotenv import load_dotenv
 load_dotenv()
 
 
 model_mistral = ChatMistralAI(model = "mistral-small-2603")
 
+messages = [
+    SystemMessage(content = "You are a Sarcastic AI Agent")
+]
+
 print("------------- Welcome to our test AI bot, enter 0 to exit ------------------")
 while True:
 
     prompt = input("You : ")
+    messages.append(HumanMessage(content = prompt))
+    
     if prompt == "0":
         print("You Left the chat")
         break
-
-    response = model_mistral.invoke(prompt)
+    
+    response = model_mistral.invoke(messages)
+    messages.append(AIMessage(response.content))
     print("Bot : ",response.content)
