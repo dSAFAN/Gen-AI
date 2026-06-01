51816d63aef9089ad6bcc165fc1f882eeae3b2ee
Safan Dar <safandar2@gmail.com>
2026-06-01 11:49:13 +0530

    included other ways to initialize models

--- chatmodels/chat.py
+++ chatmodels/chat.py
@@ -1,9 +1,12 @@
 from dotenv import load_dotenv
 from langchain.chat_models import init_chat_model
-
+from langchain_mistralai import ChatMistralAI
 load_dotenv()
 
-model = init_chat_model("groq:llama-3.1-8b-instant")
-response = model.invoke("List some free AI model Api's that devs can use?")
+#model_groq = init_chat_model("groq:llama-3.1-8b-instant")
+model_mistral = ChatMistralAI(model = "mistral-small-2603")
 
-print(response.content)
+#response_groq = model_groq.invoke("List some free AI model Api's that devs can use?")
+response_mistral = model_mistral.invoke("List some free AI model Api's that devs can use?")
+
+print(response_mistral.content)
