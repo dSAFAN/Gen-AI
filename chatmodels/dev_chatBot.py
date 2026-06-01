b5d02fb0f9a10dfcbe29d71f56aa5fe2dd6f2b54
Safan Dar <safandar2@gmail.com>
2026-06-01 15:55:45 +0530

    Added some more functionality

--- chatmodels/dev_chatBot.py
+++ chatmodels/dev_chatBot.py
@@ -5,12 +5,24 @@ load_dotenv()
 
 
 model_mistral = ChatMistralAI(model = "mistral-small-2603")
+print("Choose your AI Style")
+print("Enter 1 for Sad")
+print("Enter 2 for angry")
+print("Enter 3 for funny")
+
+choice = int(input("Enter AI Style : "))
+if choice == 1:
+    mode = "You are a sad AI agent and respond to every chat in a sad way."
+elif choice == 2:
+    mode = "You are a angry AI agent and respond to every chat in a angry way."
+elif choice == 3:
+    mode = "You are a funny AI agent and respond to every chat in a funny way."    
 
 messages = [
-    SystemMessage(content = "You are a Sarcastic AI Agent")
+    SystemMessage(content = mode )
 ]
 
-print("------------- Welcome to our test AI bot, enter 0 to exit ------------------")
+print("------------- Welcome to our test AI bsot, enter 0 to exit ------------------")
 while True:
 
     prompt = input("You : ")
