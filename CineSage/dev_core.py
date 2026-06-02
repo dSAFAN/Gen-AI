e52ec80fb277275e9de32c34a081567376309097
Safan Dar <safandar2@gmail.com>
2026-06-02 10:41:12 +0530

    Used Pydantic with prompt templates and parsers for movie extraction AI

---
+++ CineSage/dev_core.py
@@ -0,0 +1,39 @@
+from langchain_mistralai import ChatMistralAI
+from langchain_core.prompts import ChatPromptTemplate
+from pydantic import BaseModel
+from typing import List,Optional
+from langchain_core.output_parsers import PydanticOutputParser 
+from dotenv import load_dotenv
+load_dotenv()
+
+model = ChatMistralAI(model = "mistral-small-2603")
+
+#Create Schema
+class Movie(BaseModel):
+    title: str
+    release_year: Optional[int]
+    genre: List[str]
+    director: Optional[str]
+    cast: List[str]
+    rating: Optional[float]
+    summary: str
+
+#Create parser
+parser = PydanticOutputParser(pydantic_object = Movie)
+
+prompt = ChatPromptTemplate.from_messages([(
+    "system",
+"""
+    Extract movie information from the paragrapgh
+    {format_instructions}
+"""
+),
+("human","{paragraph}")
+])
+
+para = input("Provide your paragrapgh : ")
+final_prompt = prompt.invoke({"paragraph" : para, 
+                              "format_instructions": parser.get_format_instructions()})
+
+response = model.invoke(final_prompt)
+print(response.content)
